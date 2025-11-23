092e36448a80387e368844969e8af9e7f0e0b0f5
tzagaroli <237236159+tzagaroli@users.noreply.github.com>
2025-11-23 14:45:03 +0100

    Add sensors constants

---
+++ include/BBBCube_SensorsParameters.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+// ADC
+static constexpr float a1AD = -157.0f;
+static constexpr float b1AD = 0.0766602f;
+
+// Accelerometer 1 x
+static constexpr float a1x = 0.001195539868064;
+static constexpr float b1x = 0.069921487820418;
+
+// Accelerometer 1 y
+static constexpr float a1y = -0.001194484949983;
+static constexpr float b1y = 0.230998563678049;
+
+// Accelerometer 2 x
+static constexpr float a2x = 0.001194404539810;
+static constexpr float b2x = -0.354379498201718;
+
+// Accelerometer 2 y
+static constexpr float a2y = -0.001194280700135;
+static constexpr float b2y = 0.280185521539719;
+
+// Gyroscope
+static constexpr float a = 0.00106;
+static constexpr float b1z = -0.00218184638109306;
+static constexpr float b2z = 0.0905728508124077;
