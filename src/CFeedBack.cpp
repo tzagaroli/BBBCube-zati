8a67bc749bb0fef58c9186c71a5a4b7f633dbf4a
tzagaroli <237236159+tzagaroli@users.noreply.github.com>
2025-12-11 17:23:15 +0100

    V4

--- src/CFeedBack.cpp
+++ src/CFeedBack.cpp
@@ -4,7 +4,7 @@ float CFeedBack::calculate(SStateVectorData& data) const
 {
     float fRet = 0;
 
-    fRet += data.mPhi_C * K_[K_phi];
+    fRet += (data.mPhi_C + 0.0466200608116212f) * K_[K_phi];
     fRet += data.mPhi_d * K_[K_phi_d];
     fRet += data.mPsi_d * K_[K_psi_d];
 
